6dec4681fb762a8e59bbd77553bf04a8351080cc
tobywillmott <tobwil0910@highgateschool.org.uk>
2023-08-17 02:34:50 +0100

    first commit on project ever

---
+++ models.py
@@ -0,0 +1,57 @@
+from sqlalchemy import Column, Integer, Table, String, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+
+# Base is called an Abstract Base Class - Our SQL Alchemy models will inherit from this class
+Base = declarative_base()
+
+user_league = Table("user_league",
+                    Base.metadata,
+                    Column("user_league_id", Integer, primary_key=True),
+                    Column("user_id", ForeignKey("user.user_id")),
+                    Column("league_id", ForeignKey("league.league_id")),
+                    UniqueConstraint("user_id", "league_id")
+                    )
+
+
+class User(Base):
+    __tablename__ = "user"
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String, unique=False, nullable=False)
+    last_name = Column(String, unique=False, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, unique=False, nullable=False)
+
+    leagues = relationship("League",
+                           secondary=user_league,
+                           order_by="(League.league_id)",
+                           back_populates="users")
+
+
+class League(Base):
+    __tablename__ = "league"
+    league_id = Column(Integer, primary_key=True, autoincrement=True)
+    start_gameweek = Column(Integer, unique=False, nullable=False)
+    league_name = Column(String, unique=False, nullable=False)
+
+    users = relationship("League",
+                         secondary=user_league,
+                         order_by="(User.user_id)",
+                         back_populates="leagues")
+
+
+class Team(Base):
+    __tablename__ = "team"
+    team_id = Column(Integer, primary_key=True, autoincrement=True)
+    team_name = Column(String, unique=True, nullable=False)
+
+
+class Selection(Base):
+    __tablename__ = "selection"
+    selection_id = Column(Integer, primary_key=True, autoincrement=True)
+    gameweek = Column(Integer, unique=False, nullable=False)
+    outcome = Column(String, unique=False, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.user_id"), nullable=False)
+    team_id = Column(Integer, ForeignKey("team.team_id"), nullable=False)
+    league_id = Column(Integer, ForeignKey("league.league_id"), nullable=False)
+
+
---
+++ write_to_database.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import League, Team, User
+
+teams = [Team(team_name="Arsenal"),
+         Team(team_name="Aston Villa"),
+         Team(team_name="Bournemouth"),
+         Team(team_name="Brentford"),
+         Team(team_name="Brighton & Hove Albion"),
+         Team(team_name="Burnley"),
+         Team(team_name="Chelsea"),
+         Team(team_name="Crystal Palace"),
+         Team(team_name="Everton"),
+         Team(team_name="Fulham"),
+         Team(team_name="Liverpool"),
+         Team(team_name="Luton Town"),
+         Team(team_name="Manchester City"),
+         Team(team_name="Manchester United"),
+         Team(team_name="Newcastle United"),
+         Team(team_name="Nottingham Forest"),
+         Team(team_name="Sheffield United"),
+         Team(team_name="Tottenham Hotspur"),
+         Team(team_name="West Ham United"),
+         Team(team_name="Wolverhampton Wanderers")]
+
+#users = [User(first_name="Toby", last_name="Willmott", username="tobywillmott", password="P@ssword123"),
+         #User(first_name="James", last_name="Smith", username="jamessmith", password="passwordhello"),
+         #User(first_name="John", last_name="Dixon", username="tomdixon", password="thisismypassword"), ]
+
+#leagues = [League(start_gameweek=5, league_name="The best league"),
+           #League(start_gameweek=8, league_name="Tottenham Fans")]
+
+#leagues[0].users.append(users[0])
+#leagues[0].users.append(users[2])
+#leagues[1].users.append(users[1])
+#leagues[1].users.append(users[2])
+
+engine = create_engine("sqlite:///fantasy_football.db", echo=True)
+
+with Session(engine) as sess:
+    #sess.add_all(leagues)
+    sess.add_all(teams)
+    sess.commit()
